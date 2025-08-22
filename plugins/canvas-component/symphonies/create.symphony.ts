ea679a4bcfefa38a2ea0612724b90ed6fa6fdb3e
Sidney Jones <sjones@bpmsoftwaresolutions.com>
2025-08-22 17:15:36 -0400

    Updated timing to immediate for component-create.

--- plugins/canvas-component/symphonies/create.symphony.ts
+++ plugins/canvas-component/symphonies/create.symphony.ts
@@ -13,8 +13,8 @@ export const sequence = {
       beats: [
         { beat: 1, event: "canvas:component:resolve-template", title: "Resolve Template", dynamics: "mf", handler: "resolveTemplate", timing: "immediate", kind: "pure" },
           { beat: 2, event: "canvas:component:register-instance", title: "Register Instance", dynamics: "mf", handler: "registerInstance", timing: "immediate", kind: "io" },
-        { beat: 3, event: "canvas:component:create", title: "Create Node", dynamics: "mf", handler: "createNode", timing: "after-beat", kind: "stage-crew" },
-        { beat: 4, event: "canvas:component:notify-ui", title: "Notify UI", dynamics: "mf", handler: "notifyUi", timing: "after-beat", kind: "pure" },
+          { beat: 3, event: "canvas:component:create", title: "Create Node", dynamics: "mf", handler: "createNode", timing: "immediate", kind: "stage-crew" },
+          { beat: 4, event: "canvas:component:notify-ui", title: "Notify UI", dynamics: "mf", handler: "notifyUi", timing: "immediate", kind: "pure" },
       ],
     },
   ],
