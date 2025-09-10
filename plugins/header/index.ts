ae7eb9629f4de80ae28c9f0c627e807543a86dc1
Sidney Jones <sjones@bpmsoftwaresolutions.com>
2025-09-10 18:27:39 -0400

    Removed code and tests for the decoupled header.

--- plugins/header/index.ts
+++
@@ -1,9 +0,0 @@
-export { HeaderTitle } from "./ui/HeaderTitle";
-export { HeaderControls } from "./ui/HeaderControls";
-export { HeaderThemeToggle } from "./ui/HeaderThemeToggle";
-
-// Sequences not needed; registration no-op for now
-export async function register(_conductor: any) {
-  // no-op
-}
-
